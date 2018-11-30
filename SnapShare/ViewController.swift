240da4e3a8f60a3fc771eb605b40de72a0efeb00
Alex <alexandrakazaglis@gmail.com>
2018-11-30 11:30:31 -0600

    Initial Commit

---
+++ SnapShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapShare
+//
+//  Created by Alexandra Kazaglis on 11/30/18.
+//  Copyright © 2018 Alexandra Kazaglis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
